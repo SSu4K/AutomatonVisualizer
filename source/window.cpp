2f8982bb61a842010d82e87fcd2d4128bbd54149
SSu4K <ssudak.kontakt@gmail.com>
2025-08-21 13:53:30 +0200

    Added WindowSettings struct and merged properties into it.

--- source/window.cpp
+++ source/window.cpp
@@ -1,8 +1,8 @@
 #include "window.h"
 #include <iostream>
 
-static Gradient g1({sf::Color::Yellow, sf::Color::Red,
-                    sf::Color::Cyan, sf::Color::Yellow},
+static Gradient g1({sf::Color::Yellow, sf::Color::Red, sf::Color::Cyan,
+                    sf::Color::Yellow},
                    PERIODIC);
 static QuantizedGradient g2(g1, 20);
 
@@ -39,89 +39,93 @@ sf::Color SimulationWindow::get_color(bool value, bool loaded, size_t age) {
   return g2.get_color_by_int(age);
 }
 
-vector<sf::Color> SimulationWindow::get_color_array(){
+vector<sf::Color> SimulationWindow::get_color_array() {
   vector<sf::Color> result;
-  for(size_t i=0; i<simulation->get_cell_count(); i++){
-    sf::Color color = get_color(simulation->value_buffer[i], simulation->loaded_buffer[i],
+  for (size_t i = 0; i < simulation->get_cell_count(); i++) {
+    sf::Color color =
+        get_color(simulation->value_buffer[i], simulation->loaded_buffer[i],
                   simulation->age_buffer[i]);
     result.push_back(color);
     color.toInteger();
-    //cout << hex << color.toInteger() << endl;
   }
-  // std::cout << dec << result.size() << endl;
-
   return result;
 }
 
-void SimulationWindow::create_vertex_array(){
-  vertex_array = sf::VertexArray(sf::PrimitiveType::Quads, 4*simulation->get_cell_count());
+void SimulationWindow::create_vertex_array() {
+  vertex_array = sf::VertexArray(sf::PrimitiveType::Quads,
+                                 4 * simulation->get_cell_count());
   const int width = simulation->get_size()[0];
   const int height = simulation->get_size()[1];
   float x, y;
   for (int i = 0; i < width * height; i++) {
     x = i % width;
     y = i / width;
-    vertex_array[4*i + 0] = sf::Vertex({x,   y},    sf::Color::Black);
-    vertex_array[4*i + 1] = sf::Vertex({x+1, y},    sf::Color::Black);
-    vertex_array[4*i + 2] = sf::Vertex({x+1, y+1},  sf::Color::Black);
-    vertex_array[4*i + 3] = sf::Vertex({x, y+1},    sf::Color::Black);
+    vertex_array[4 * i + 0] = sf::Vertex({x, y}, sf::Color::Black);
+    vertex_array[4 * i + 1] = sf::Vertex({x + 1, y}, sf::Color::Black);
+    vertex_array[4 * i + 2] = sf::Vertex({x + 1, y + 1}, sf::Color::Black);
+    vertex_array[4 * i + 3] = sf::Vertex({x, y + 1}, sf::Color::Black);
   }
-  // std::cout << vertex_array.getVertexCount() << endl;
 }
 
 void SimulationWindow::update_vertex_array(double t) {
-  for (size_t i = 0; i<simulation->get_cell_count(); i++) {
+  for (size_t i = 0; i < simulation->get_cell_count(); i++) {
     sf::Color color = interp_func(old_color_array[i], new_color_array[i], t);
-    vertex_array[4*i + 0].color = color;
-    vertex_array[4*i + 1].color = color;
-    vertex_array[4*i + 2].color = color;
-    vertex_array[4*i + 3].color = color;
+    vertex_array[4 * i + 0].color = color;
+    vertex_array[4 * i + 1].color = color;
+    vertex_array[4 * i + 2].color = color;
+    vertex_array[4 * i + 3].color = color;
   }
 }
 
-SimulationWindow::SimulationWindow(sf::Vector2i window_size,
-                                   shared_ptr<Simulation> simulation,
-                                   unsigned int rate,
-                                   unsigned int srate)
+SimulationWindow::SimulationWindow(const sf::Vector2i window_size,
+                                   const shared_ptr<Simulation> simulation,
+                                   const WindowSettings& settings)
     : sf::RenderWindow(sf::VideoMode(window_size.x, window_size.y),
                        "Simulation"),
       simulation(simulation),
-      framerate(rate),
-      simulation_framerate(srate),
-      frames_per_step(rate / srate),
+      settings(settings),
       frame_counter(0),
-      max_view_speed(100),
-      drag_force(2000),
-      show_grid(false),
       do_update_view(false),
       do_redraw(false),
       paused(true) {
-  if (frames_per_step <= 0) {
+  if (settings.simulation_framerate > 0) {
+    frames_per_step = settings.framerate / settings.simulation_framerate;
+    if (frames_per_step <= 0) {
+      frames_per_step = 1;
+    }
+  } else {
     frames_per_step = 1;
   }
-  this->setFramerateLimit(framerate);
+
+  this->setFramerateLimit(settings.framerate);
   sf::Vector2i simulation_size = {(int)simulation->get_size()[0],
                                   (int)simulation->get_size()[1]};
   float w_ratio = simulation_size.x / float(window_size.x);
   float h_ratio = simulation_size.y / float(window_size.y);
 
   sf::Vector2f center = sf::Vector2f(simulation_size) / 2.0f;
-  view = sf::View(center, sf::Vector2f(window_size)), view.zoom(std::max(w_ratio, h_ratio));
+  view = sf::View(center, sf::Vector2f(window_size)),
+  view.zoom(std::max(w_ratio, h_ratio));
   setView(view);
 
-  view_body =
-      Body::Body(50, 10, Body::calculate_resistance(drag_force, max_view_speed),
-                 0.01 * max_view_speed);
+  view_body = Body::Body(
+      50, 10,
+      Body::calculate_resistance(settings.drag_force, settings.max_view_speed),
+      0.01 * settings.max_view_speed);
   view_body.set_pos(center);
 
   new_color_array = get_color_array();
   old_color_array = new_color_array;
   create_vertex_array();
-  
-  grid = generate_grid(simulation_size, sf::Color(40, 40, 40));
+
+  grid = generate_grid(simulation_size, sf::Color(0, 0, 0));
 
   this->simulation->reset();
 }
+
+SimulationWindow::SimulationWindow(const sf::Vector2i window_size,
+                                   const shared_ptr<Simulation> simulation)
+    : SimulationWindow(window_size, simulation, DEFAULT_SETTINGS) {}
 
 void SimulationWindow::handle_input() {
   inputSystem.update();
@@ -152,27 +156,25 @@ void SimulationWindow::step() {
     simulation->step();
     old_color_array = vector<sf::Color>(new_color_array);
     new_color_array = get_color_array();
-    //update_vertex_array(0);
   }
 
-  if(!paused){
+  if (!paused) {
     double t = double(frame_counter) / frames_per_step;
-    update_vertex_array(t*t);
+    update_vertex_array(t * t);
   }
-  
+
   clear();
   draw(vertex_array);
-  if(show_grid){
+  if (settings.show_grid) {
     draw(grid);
   }
-    
 
   display();
   frame_counter++;
   frame_counter %= frames_per_step;
 
-  float dt = 1.0 / framerate;
-  view_body.simulate(dt, drag_force * direction_vector);
+  float dt = 1.0 / settings.framerate;
+  view_body.simulate(dt, settings.drag_force * direction_vector);
   view.setCenter(view_body.get_pos());
   this->update_view();
 }
--- source/window.h
+++ source/window.h
@@ -4,19 +4,30 @@
 #include <SFML/Graphics.hpp>
 #include <random>
 
-#include "utils.hpp"
 #include "body.h"
 #include "colors/gradient.h"
 #include "input/inputsystem.h"
 #include "simulation.h"
+#include "utils.hpp"
 
 #define DEFAULT_FRAMERATE 60
 #define DEFAULT_SIMULATION_FRAMERATE 10
 
+struct WindowSettings {
+  int framerate;
+  int simulation_framerate;
+  bool show_grid;
+  float max_view_speed;
+  float drag_force;
+};
+
+const WindowSettings DEFAULT_SETTINGS = {
+    DEFAULT_FRAMERATE, DEFAULT_SIMULATION_FRAMERATE, false, 100, 2000};
+
 class SimulationWindow : public sf::RenderWindow {
  private:
   shared_ptr<Simulation> simulation;
-  
+
   sf::View view;
   sf::VertexArray vertex_array;
   sf::VertexArray grid;
@@ -26,26 +37,24 @@ class SimulationWindow : public sf::RenderWindow {
 
   InputSystem inputSystem;
 
-  int framerate;
-  int simulation_framerate;
+  WindowSettings settings;
+
   int frames_per_step;
   int frame_counter;
 
-  float max_view_speed;
-  float drag_force;
   sf::Vector2f direction_vector;
   Body::Body view_body;
 
-  function<sf::Color(const sf::Color&, const sf::Color&, float)> interp_func = utils::powrp<3>;
+  function<sf::Color(const sf::Color&, const sf::Color&, float)> interp_func =
+      utils::powrp<3>;
 
-  bool show_grid;
   bool do_update_view;
   bool do_redraw;
   bool paused;
 
   void update_view();
   sf::Color get_color(bool value, bool loaded, size_t age);
-  
+
   vector<sf::Color> get_color_array();
   void create_vertex_array();
   void update_vertex_array(double t);
@@ -54,10 +63,10 @@ class SimulationWindow : public sf::RenderWindow {
 
  public:
   SimulationWindow() = default;
-  SimulationWindow(sf::Vector2i window_size,
-                   shared_ptr<Simulation> simulation,
-                   unsigned int rate = DEFAULT_FRAMERATE,
-                   unsigned int srate = DEFAULT_SIMULATION_FRAMERATE);
+  SimulationWindow(const sf::Vector2i window_size, const shared_ptr<Simulation> simulation);
+  SimulationWindow(const sf::Vector2i window_size,
+                   const shared_ptr<Simulation> simulation,
+                   const WindowSettings &settings);
 
   void step();
 };
